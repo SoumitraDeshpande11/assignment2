d6dda322b89be5e657cc69acd9560dddc17a23ec
Soumitra Deshpande <soumitradesh11@gmail.com>
2024-10-28 23:42:36 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Return the difference of two numbers."""
+    return a - b
+
+def multiply(a, b):
+    """Return the product of two numbers."""
+    return a * b
+
+def divide(a, b):
+    """Return the quotient of two numbers."""
+    if b == 0:
+        return "Error: Division by zero."
+    return a / b
+add(10,20)
